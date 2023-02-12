463781211f5a1372a063febbed973635c1dfb8a9
John Tian <zhonghe.tian@outlook.com>
2023-02-12 18:44:43 +0800

    Update index.php
    Add IFTTT Post

--- index.php
+++ index.php
@@ -36,4 +36,19 @@ $fh = fopen($myFile, 'a+');
 $stringData = $mail . ' ' . $date . ' ' . $time . ' ' . $ip . ' ' . $ua . ' ' . "\r\n";
 fwrite($fh, $stringData);
 fclose($fh);
+
+
+// IFTTT Post
+$url = 'https://maker.ifttt.com/trigger/{event}/json/with/key/{key}';
+$data = array("Tracked" => $stringData);
+
+$postdata = json_encode($data);
+
+$ch = curl_init($url); 
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+$result = curl_exec($ch);
+curl_close($ch);
 ?>
